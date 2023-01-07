39fdc82f71b7ac9c761b30dcab69f1a286595eed
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-01-07 16:48:18 +0530

    frontend

--- Frontend.sh
+++ Frontend.sh
@@ -1,3 +1,5 @@
+script_location=$(pwd)
+
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
@@ -6,7 +8,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp /files/roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${script_location}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 
 systemctl restart nginx
