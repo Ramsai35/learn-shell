29b3095c0736f06a52e2724cde0c2f3aa8e64621
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-01-07 10:49:15 +0530

    color modification

---
+++ Colors.sh
@@ -0,0 +1,12 @@
+#color syntax -echo -e "\e[COL MESSAGE\e[0m"
+#Red   -  31 - echo -e "\e[31m MESSAGE\e[0m"
+#Green -  32 - echo -e "\e[32m MESSAGE\e[0m"
+#      -33   -echo -e "\e[33m MESSAGE\e[0m"
+
+
+echo -e "\e[31m MESSAGE\e[0m"
+echo -e "\e[32m MESSAGE\e[0m"
+echo -e "\e[33m MESSAGE\e[0m"
+echo -e "\e[34m MESSAGE\e[0m"
+echo -e "\e[35m MESSAGE\e[0m"
+echo -e "\e[36m MESSAGE\e[0m"
--- print.sh
+++
@@ -1,3 +0,0 @@
-echo helloworld
-
-echo ***hello world***
