0429a82cc4f1c97da9c820ff4145d65f104614d9
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-01-08 15:04:53 +0530

    frontend

--- Mongo.sh
+++ Mongo.sh
@@ -1,12 +1,21 @@
-script_location=$(pwd)
+source common.sh
 
-cp ${script_location}/files/mongo.conf /etc/yum.repos.d/mongo.repo
+echo -e "\e[35m Changing direction command\e[0m"
+cp ${script_location}/files/mongo.conf /etc/yum.repos.d/mongo.repo &>>${LOG}
+status_check
 
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
+echo -e "\e[35m Changing direction command\e[0m"
+yum install mongodb-org -y &>>${LOG}
+status_check
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo -e "\e[35m Changing Port status\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
+status_check
 
-systemctl restart mongod
+echo -e "\e[35m enable Mongodb\e[0m"
+systemctl enable mongod &>>${LOG}
+status_check
 
+echo -e "\e[35m start Mongodb\e[0m"
+systemctl start mongod &>>${LOG}
+status_check
