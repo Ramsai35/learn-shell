e42520c72934b39b48edd6759d1e2bb20a860b20
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-01-08 07:03:25 +0530

    frontend

--- Frontend.sh
+++ Frontend.sh
@@ -12,28 +12,58 @@ fi
 
 echo -e '\e[32m enable nginx\e[0m'
 systemctl enable nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+   then
+      echo success
+   else
+      echo Fail
+fi
 
 echo -e '\e[32m start nginx\e[0m'
 systemctl start nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+   then
+      echo success
+   else
+      echo Fail
+fi
 
 echo -e '\e[32m cleanup nginx\e[0m'
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+   then
+      echo success
+   else
+      echo Fail
+fi
 
 echo -e '\e[32m downloading webcontent\e[0m'
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+   then
+      echo success
+   else
+      echo Fail
+fi
 
 cd /usr/share/nginx/html &>>${LOG}
 
 echo -e '\e[32m unzip webcontent\e[0m'
 unzip /tmp/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+   then
+      echo success
+   else
+      echo Fail
+fi
 
 cp ${script_location}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 
 echo -e '\e[32m restart nginx\e[0m'
 systemctl restart nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ];
+   then
+      echo success
+   else
+      echo Fail
+fi
