5b5a77f750d7afe731572b449b4d9f3727aadc2d
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-01-08 14:51:17 +0530

    frontend

--- Frontend.sh
+++ Frontend.sh
@@ -1,69 +1,36 @@
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
-
+source common.sh
 echo -e '\e[32m install nginx\e[0m'
 yum install nginx -y &>>${LOG}
-if [ $? -eq 0 ];
-   then
-      echo success
-   else
-      echo Fail
-fi
+status_check
 
 echo -e '\e[32m enable nginx\e[0m'
 systemctl enable nginx &>>${LOG}
-if [ $? -eq 0 ];
-   then
-      echo success
-   else
-      echo Fail
-fi
+status_check
 
 echo -e '\e[32m start nginx\e[0m'
 systemctl start nginx &>>${LOG}
-if [ $? -eq 0 ];
-   then
-      echo success
-   else
-      echo Fail
-fi
+status_check
 
 echo -e '\e[32m cleanup nginx\e[0m'
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-if [ $? -eq 0 ];
-   then
-      echo success
-   else
-      echo Fail
-fi
+status_check
 
 echo -e '\e[32m downloading webcontent\e[0m'
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-if [ $? -eq 0 ];
-   then
-      echo success
-   else
-      echo Fail
-fi
+status_check
 
+echo -e '\e[32m downloading webcontent\e[0m'
 cd /usr/share/nginx/html &>>${LOG}
+status_check
 
 echo -e '\e[32m unzip webcontent\e[0m'
 unzip /tmp/frontend.zip &>>${LOG}
-if [ $? -eq 0 ];
-   then
-      echo success
-   else
-      echo Fail
-fi
+status_check
 
+echo -e '\e[32m changing direction\e[0m'
 cp ${script_location}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+status_check
 
 echo -e '\e[32m restart nginx\e[0m'
 systemctl restart nginx &>>${LOG}
-if [ $? -eq 0 ];
-   then
-      echo success
-   else
-      echo Fail
-fi
+status_check
